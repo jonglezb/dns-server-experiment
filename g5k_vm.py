8e932f6f15ab720a729cb9a1ad8434101ee64dd4
Baptiste Jonglez <baptiste.jonglez@imag.fr>
2017-12-21 18:13:13 +0100

    Update unbound git repository before running experiment

--- g5k_vm.py
+++ g5k_vm.py
@@ -262,6 +262,11 @@ sysctl net.core.somaxconn=100000 || exit 1
 sysctl net.ipv4.tcp_max_syn_backlog=100000 || exit 1
 sysctl fs.file-max=20000000 || exit 1
 echo 20000000 > /proc/sys/fs/nr_open || exit 1
+
+# Update git repository for unbound.
+cd /root/unbound || exit 1
+git pull || exit 1
+make -j8 || exit 1
         """.format(vm_subnet=self.subnet)
         task = execo.Remote(script, [self.server],
                             connection_params=self.server_conn_params,
