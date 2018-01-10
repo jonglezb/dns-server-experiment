jonglezb/dns-server-experiment

commit 87af078f5d829fb9c5d5009fab6f2ae0d7b6a61f

Log output of unbound

g5k_vm.py
@@ -392,6 +392,7 @@ EOF
 
     def run(self):
         rtt_file = self.result_dir + "/rtt.csv"
+        unbound = None
         try:
             self.reserve_subnet()
             self.reserve_vmhosts()
@@ -442,17 +443,21 @@ EOF
                                 # Expect: connection ID, timestamp, RTT
                                 data = line.split(",")
                                 rtt.writerow([client_id, int(data[0]), float(data[1]), int(data[2])])
-                #unbound.wait()
-                #self.vm_process.wait()
         except Exception as e:
             logger.error("Exception raised: {}\n{}".format(e, format_exc()))
         finally:
             #self.kill_all_vm()
             if self.vm_process:
                 self.vm_process.kill()
+            if unbound:
+                unbound.kill()
+                logger.debug("Waiting for unbound to exit")
+                self.unbound.wait()
+                self.log_output(unbound, "unbound")
+            if self.vm_process:
                 logger.debug("Waiting for VM to exit")
                 self.vm_process.wait()
-                logger.info("All VM shut down")
+                logger.info("Unbound an all VMs are shut down")
                 self.log_output(self.vm_process, "vm_process")
                 print(execo.Report([self.vm_process]).to_string())
             #for s in self.vm_process.processes:

commit 92132a5c2e25a4611ada0233a3b5acdb81ad5834

Fix crash when stopping unbound (typo on variable name)
Fixes: 87af078f5d ("Log output of unbound")

g5k_vm.py
@@ -470,7 +470,7 @@ EOF
             if unbound:
                 unbound.kill()
                 logger.debug("Waiting for unbound to exit")
-                self.unbound.wait()
+                unbound.wait()
                 self.log_output(unbound, "unbound")
             if self.vm_process:
                 logger.debug("Waiting for VM to exit")